2bdd7c3b33a8c702fec300b44aa0862a6ce3e1a7
“pratik” <pratikbhandurge2222@gmail.com>
2022-08-16 02:04:59 +0530

    Uc6Solution

--- AddressBookProgram/AddressBook.cs
+++ AddressBookProgram/AddressBook.cs
@@ -16,20 +16,36 @@ namespace AddressBookProgram
         public long zip;
         public long phoneNumber;
         public String email;
+        public AddressBook(string firstName, String lastName, String address, String city, String state, long zip, long phoneNumber, String email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public AddressBook()
+        {
+
+        }
 
         static public void SetAddress()
         {
 
             AddressBook addressBook = new AddressBook();
-            addressBook.firstName = "Akshay";
-            addressBook.lastName = "Bhagwat";
-            addressBook.address = "Katraj Pune";
+            addressBook.firstName = "Pratik";
+            addressBook.lastName = "Bhandurge";
+            addressBook.address = "Vimannager Pune";
             addressBook.city = "Pune";
             addressBook.state = "Maharashtra";
-            addressBook.zip = 12354;
-            addressBook.phoneNumber = 6789009876;
-            addressBook.email = "akshayb407@gmail.com";
-            Console.WriteLine(addressBook.firstName);
+            addressBook.zip = 111111;
+            addressBook.phoneNumber = 9876543210;
+            addressBook.email = "pratik2121@gmail.com";
+
         }
     }
 }
